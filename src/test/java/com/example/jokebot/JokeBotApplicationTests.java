94d44b6ccc6863c7a232d3ad338775fa2eeb2bba
Hlib Chibichian <chibgleb99@gmail.com>
2023-12-24 13:51:22 +0000

    s

---
+++ src/test/java/com/example/jokebot/JokeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jokebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
